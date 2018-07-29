36c480c7d451904daed9bc86c544b7e0607fdfba
Sam Ashtar <samashtarm@gmail.com>
2018-07-29 01:10:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,6 @@ def speak_to_grandma(phrase)
     puts "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  else phrase
+  else 
     puts "HUH?! SPEAK UP, SONNY!"
   end
